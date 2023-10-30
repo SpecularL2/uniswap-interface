1f8870adc1f6954bc972d5b7212561a04642d169
“StefanVelkoski” <velkoskis@gmail.com>
2023-10-30 15:54:03 +0100

    fix: add specular tokenlist

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -2,20 +2,5 @@
 export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-  DEFAULT_TOKEN_LIST_URL,
-  't2crtokens.eth', // kleros
-  'tokens.1inch.eth', // 1inch
-  'synths.snx.eth',
-  'tokenlist.dharma.eth',
-  'defi.cmc.eth',
-  'erc20.cmc.eth',
-  'stablecoin.cmc.eth',
-  'tokenlist.zerion.eth',
-  'tokenlist.aave.eth',
-  'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
-  'https://app.tryroll.com/tokens.json',
-  'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
-  'https://defiprime.com/defiprime.tokenlist.json',
-  'https://umaproject.org/uma.tokenlist.json',
-  'https://gist.githubusercontent.com/alexshliu/b3119bdfbee6ef2a6b83297b3fd7aff5/raw/aa417ca4369405fd52d23a071cfaec8787bd44b4/tokens.json'
+  'https://specularl2.github.io/specular-token-list/specular.tokenlist.json'
 ]
